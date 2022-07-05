339a83a8dba9446c1783a444ab04f71622c0719f
Александр Голубенко <webgolub@gmail.com>
2022-07-05 15:49:43 +0300

    Реализовал показ/скрытие попапа редактирования изображения для загрузки

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 import {renderThumbnails} from './render-thumbnails.js';
 import {createPhotos} from './data.js';
+import './upload-form.js';
+import './validate.js';
 
 // Создание массива объектов с «рыбой» фотографий
 const somePhotos = createPhotos(25);
