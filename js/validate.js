eca6fb12936f925c4e6fb0b26c50d2a1d1ad4b9c
Александр Голубенко <webgolub@gmail.com>
2022-07-05 17:03:37 +0300

    Добавил валидацию поля ввода комментария

--- js/validate.js
+++ js/validate.js
@@ -1,1 +1,20 @@
-// Валидация форм validate.js
+// Форма загрузки изображения на сайт
+const uploadForm = document.querySelector('.img-upload__form');
+// Попап редактирования загружаемого изображения
+const imgUploadOverlay = uploadForm.querySelector('.img-upload__overlay');
+// Поле ввода комментария
+const commentInput = imgUploadOverlay.querySelector('.text__description');
+
+const pristine = new Pristine (uploadForm, {
+  classTo: 'img-upload__form',
+  errorClass: 'form-item__invalid',
+  successClass: 'form-item__valid',
+  errorTextParent: 'img-upload__field-wrapper:last-child',
+  errorTextTag: 'div',
+  errorTextClass: 'form__error'
+});
+
+uploadForm.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+  pristine.validate();
+});
