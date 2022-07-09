e0a0269e48b7ff3c7f70b6dd6dfe2c566acb31fc
Александр Голубенко <webgolub@gmail.com>
2022-07-09 16:25:45 +0300

    Переписал editor-scale.js на работу с коллбеком из editor

--- js/editor-scale.js
+++ js/editor-scale.js
@@ -4,36 +4,42 @@ const ScaleValue = {
   STEP: 25,
 };
 
-// Форма загрузки изображения на сайт
-const uploadForm = document.querySelector('.img-upload__form');
 // Поле вывода масштаба
-const scaleValueDisplay = uploadForm.querySelector('.scale__control--value');
-// Превью редактируемого изображения
-const imgPreview = uploadForm.querySelector('.img-upload__preview img');
-// Функция полученя целого числа из значения поля
-const getFieldIntegerValue = (field) => Number(field.value.split('%')[0]);
+const scaleValueDisplay = document.querySelector('.scale__control--value');
+// Кнопка увеличения масштаба
+const scaleUpButton = document.querySelector('.scale__control--bigger');
+//Кнопка уменьшения масштаба
+const scaleDownButton = document.querySelector('.scale__control--smaller');
 
-// Функция проверки что значение масштаба в допустимом диапазоне
-const isScaleValueInRange = () =>
-  getFieldIntegerValue(scaleValueDisplay) >= ScaleValue.MIN &&
-  getFieldIntegerValue(scaleValueDisplay) <= ScaleValue.MAX;
+let currentScale = ScaleValue.MAX;
+let scaleChangeCallback = null;
 
-// Функция изменения масштаба по клику на кнопки
-const onScaleButtonsClick = (evt) => {
-  let inicialScaleValue = getFieldIntegerValue(scaleValueDisplay);
-
-  if (isScaleValueInRange()) {
-
-    if (evt.target.classList.contains('scale__control--bigger') &&
-    inicialScaleValue !== ScaleValue.MAX) {
-      scaleValueDisplay.value =  `${inicialScaleValue += ScaleValue.STEP}%`;
-
-    } else if (evt.target.classList.contains('scale__control--smaller') &&
-    inicialScaleValue !== ScaleValue.MIN) {
-      scaleValueDisplay.value = `${inicialScaleValue -= ScaleValue.STEP}%`;
-    }
-    imgPreview.style.transform =`scale(${(inicialScaleValue * 0.01)}`;
-  }
+const setScaleChangeHandler = (callback) => {
+  scaleChangeCallback = callback;
 };
 
-export {onScaleButtonsClick};
+const resetScale = () => {
+  scaleValueDisplay.value = '100%';
+};
+
+const renderScale = (value) => {
+  currentScale = value;
+  scaleValueDisplay.value = `${value}%`;
+  scaleChangeCallback(value);
+};
+
+scaleUpButton.addEventListener('click', () => {
+  const nextScale = currentScale + ScaleValue.STEP;
+  if (nextScale <= ScaleValue.MAX) {
+    renderScale(nextScale);
+  }
+});
+
+scaleDownButton.addEventListener('click', () => {
+  const nextScale = currentScale - ScaleValue.STEP;
+  if (nextScale >= ScaleValue.MIN) {
+    renderScale(nextScale);
+  }
+});
+
+export {resetScale, setScaleChangeHandler};
--- js/editor.js
+++ js/editor.js
@@ -1,5 +1,5 @@
 import { isEscKey } from './util.js';
-import { onScaleButtonsClick } from './editor-scale.js';
+import { resetScale, setScaleChangeHandler } from './editor-scale.js';
 import './editor-effects.js';
 
 // Значения атрибута name для текстовых полей
@@ -12,12 +12,19 @@ const uploadFileInput = uploadForm.querySelector('#upload-file');
 const imgUploadOverlay = uploadForm.querySelector('.img-upload__overlay');
 // Кнопка закрытия оверлея
 const imgUploadOverlayCancellButton = uploadForm.querySelector('#upload-cancel');
-// Поле вывода масштаба
-const scaleValueDisplay = uploadForm.querySelector('.scale__control--value');
-// Группа полей для управления масштабом
-const scaleInputsFieldset = uploadForm.querySelector('.img-upload__scale');
-
+// Превью редактируемого изображения
+const imgPreview = uploadForm.querySelector('.img-upload__preview img');
+// Проверка что фокус не на текстовых полях
 const isNotTextFields = (evt) => ! TEXT_FIELD_NAMES.includes(evt.target.name);
+
+// Функция сброса превью загружаемого изображения
+const resetUploadPicture = () => {
+  resetScale();
+};
+// Функция установки коллбэка для функции отрисовки изменения масштаба
+setScaleChangeHandler((value) => {
+  imgPreview.style.transform = `scale(${value / 100 })`;
+});
 
 // Обработчик клика по кнопке закрытия оверлея редактирования загружаемого изображения
 const onImgUploadOverlayCancelButtonClick = (evt) => {
@@ -42,16 +49,14 @@ function closeUploadOverlay () {
   uploadForm.reset();
   document.removeEventListener('keydown', onImgUploadOverlayEscKeydown);
   imgUploadOverlayCancellButton.removeEventListener('click', onImgUploadOverlayCancelButtonClick);
-  scaleInputsFieldset.removeEventListener('click', onScaleButtonsClick);
 }
 
 // Событие изменения поля загрузки изображения
 uploadFileInput.addEventListener('change', () => {
   imgUploadOverlay.classList.remove('hidden');
   document.body.classList.add('modal-open');
-  scaleValueDisplay.value =  '100%';
+  resetUploadPicture();
 
   document.addEventListener('keydown', onImgUploadOverlayEscKeydown);
   imgUploadOverlayCancellButton.addEventListener('click', onImgUploadOverlayCancelButtonClick);
-  scaleInputsFieldset.addEventListener('click', onScaleButtonsClick);
 });
